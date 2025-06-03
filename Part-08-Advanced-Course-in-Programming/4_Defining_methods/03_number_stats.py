ea97d488f0c73ac6d1af7f26e8e088880ad71e84
Madalena <mmadalenaandrade93@gmail.com>
2025-06-03 13:38:04 +0100

    Update 03_number_stats.py

--- Part-08-Advanced-Course-in-Programming/4_Defining_methods/03_number_stats.py
+++ Part-08-Advanced-Course-in-Programming/4_Defining_methods/03_number_stats.py
@@ -1,48 +1,38 @@
 # Write your solution here!
 class  NumberStats:
     def __init__(self):
-        self.numbers = 0
-        self.count = 0
+        self.numbers = []
 
     def add_number(self, number:int):
-        self.numbers += number
-        self.count += 1
+        self.numbers.append(number)
 
     def count_numbers(self):
-        return self.count
+        return len(self.numbers)
     
     def get_sum(self):
-        return self.numbers
-    
+        return 0 if not self.numbers else sum(self.numbers)
+
     def average(self):
-        if self.count > 0:
-            return self.numbers/self.count
-        else:
-            return self.numbers
-
-
-stats = NumberStats()
-even_stats = NumberStats()
-odd_stats = NumberStats()
+        return 0 if not self.numbers else self.get_sum()/self.count_numbers()
 
 print("Please type in integer numbers:")
-while True:
-    number = int(input(""))
-    if number == -1:
-        break
-    if number %2 == 0:
-        even_stats.add_number(number)
-        stats.add_number(number)
-    else:
-        odd_stats.add_number(number)
-        stats.add_number(number)
-print(f"Sum of numbers: {stats.get_sum()}")
-print(f"Mean of numbers: {stats.average()}")
-print(f"Sum of even numbers: {even_stats.get_sum()}")
-print(f"Sum of odd numbers: {odd_stats.get_sum()}")
+numbers = NumberStats()
+even_numbers = NumberStats()
+odd_numbers = NumberStats()
+number = 0
+while number != -1:
+    try:
+        number = int(input())
+        if number != -1:
+            numbers.add_number(number)
+            even_numbers.add_number(number) if number % 2 == 0 else odd_numbers.add_number(number)
+    except:
+        print("Please insert a valid integer")
 
-
-
-
+print(f"Sum of numbers: {numbers.get_sum()}")
+print(f"Mean of numbers: {numbers.average():.2f}")
+print(f"Sum of even numbers: {even_numbers.get_sum()}")
+print(f"Sum of odd numbers: {odd_numbers.get_sum()}")
+    
 
 
