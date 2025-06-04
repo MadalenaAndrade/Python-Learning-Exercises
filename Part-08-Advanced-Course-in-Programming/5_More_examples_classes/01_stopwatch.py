b047c87fee4e94510d711a876858dcb769f9cc75
Madalena <mmadalenaandrade93@gmail.com>
2025-06-04 13:08:05 +0100

    Update 01_stopwatch.py

--- Part-08-Advanced-Course-in-Programming/5_More_examples_classes/01_stopwatch.py
+++ Part-08-Advanced-Course-in-Programming/5_More_examples_classes/01_stopwatch.py
@@ -1,34 +1,23 @@
 # Write your solution here:
-from datetime import time
 class Stopwatch:
     def __init__(self):
         self.seconds = 0
         self.minutes = 0
 
     def tick(self):
-        if self.seconds == 59 and self.minutes == 59:
-            self.seconds, self.minutes = (0, 0)
-        elif self.seconds < 59:
-            self.seconds += 1
-        elif self.seconds == 59:
-            self.minutes += 1
+        self.seconds += 1
+        if self.seconds == 60:
             self.seconds = 0
-
+            self.minutes += 1
+            if self.minutes == 60:
+                self.minutes = 0
+           
     
-
     def __str__(self):
-        if self.minutes < 10 and self.seconds < 10:
-            return f"0{self.minutes}:0{self.seconds}"
-        elif self.minutes < 10:
-            return f"0{self.minutes}:{self.seconds}"
-        elif self.seconds < 10:
-            return f"{self.minutes}:0{self.seconds}"
-        else:
-            return f"{self.minutes}:{self.seconds}"
+        return f"{self.minutes:02d}:{self.seconds:02d}"
 
-if __name__ == "__main__":
+if __name__=="__main__":
     watch = Stopwatch()
-    for i in range(130):
+    for i in range(3000):
         print(watch)
         watch.tick()
-        
