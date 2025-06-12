7deb35013365a2d85d9d7979d3698a0408d5b4b8
Madalena <mmadalenaandrade93@gmail.com>
2025-06-12 19:12:24 +0100

    Update 01_code_1.py

--- Part-09-Advanced-Course-in-Programming/6_More_Examples/01_code_1.py
+++ Part-09-Advanced-Course-in-Programming/6_More_Examples/01_code_1.py
@@ -1,74 +1,77 @@
-# I did more than three instances which the exercise explicity said to don't do. This has now been corrected similarly(with some but small differences) to the model solution:
+# Write your solution here:
 class Item:
     def __init__(self, name: str, weight: int):
         self.__name = name
         self.__weight = weight
-    
-    def __str__(self):
-        return f"{self.__name} ({self.__weight} kg)"
     
     def name(self):
         return self.__name
     
     def weight(self):
         return self.__weight
-
+    
+    def __str__(self):
+        return f"{self.name()} ({self.weight()} kg)"
+    
 class Suitcase:
     def __init__(self, max_weight: int):
         self.__max_weight = max_weight
-        self.__packed_items = []
+        self.__items = []
     
     def weight(self):
-        present_weight = 0
-        for item in self.__packed_items:
-            present_weight += item.weight()
-        return present_weight
-    
-    def __str__(self):
-        item_term = "s" if len(self.__packed_items) != 1 else ""
-        return f"{len(self.__packed_items)} item{item_term} ({self.weight()} kg)"
-    
+        weight = 0
+        if self.__items:
+            for current_item in self.__items:
+                weight += current_item.weight()
+        
+        return weight
+
     def add_item(self, item: Item):
-        if item.weight() + self.weight() <= self.__max_weight:
-            self.__packed_items.append(item)
+        if self.weight() + item.weight() <= self.__max_weight:
+            self.__items.append(item)
 
     def print_items(self):
-        for item in self.__packed_items:
+        for item in self.__items:
             print(item)
-    
-    def heaviest_item(self): 
-        heaviest = 0
-        if self.__packed_items == []:
-            heaviest_item = None
-        else:
-            for item in self.__packed_items:
-                if item.weight() > heaviest:
-                    heaviest = item.weight()
-                    heaviest_item = item 
-        return heaviest_item
+
+    def heaviest_item(self):
+        if not self.__items:
+            return None
+        
+        heaviest = self.__items[0]
+        for item in self.__items:
+            if item.weight() > heaviest.weight():
+                heaviest = item
+        return heaviest
+
+    def __str__(self):
+        item_synthax = "item" if len(self.__items) == 1 else "items"
+        return f"{len(self.__items)} {item_synthax} ({self.weight()} kg)"
 
 class CargoHold:
     def __init__(self, max_weight: int):
         self.__max_weight = max_weight
         self.__suitcases = []
-    
-    def weight(self):
-        current_weight = 0
-        for suitcase in self.__suitcases:
-            current_weight += suitcase.weight()
-        return current_weight
 
-    def __str__(self):
-        end_term = "s" if len(self.__suitcases) != 1 else ""
-        return f"{len(self.__suitcases)} suitcase{end_term}, space for {self.__max_weight-self.weight()} kg"
+    def weight(self):
+        weight = 0
+        if self.__suitcases:
+            for current_suitcase in self.__suitcases:
+                weight += current_suitcase.weight()
+        
+        return weight
     
     def add_suitcase(self, suitcase: Suitcase):
-        if suitcase.weight() + self.weight() <= self.__max_weight:
+        if self.weight() + suitcase.weight() <= self.__max_weight:
             self.__suitcases.append(suitcase)
-    
+
     def print_items(self):
         for suitcase in self.__suitcases:
             suitcase.print_items()
+
+    def __str__(self):
+        suitcase_synthax = "suitcase" if len(self.__suitcases) == 1 else "suitcases"
+        return f"{len(self.__suitcases)} {suitcase_synthax}, space for {self.__max_weight-self.weight()} kg"
 
 if __name__=="__main__":
     book = Item("ABC Book", 2)
