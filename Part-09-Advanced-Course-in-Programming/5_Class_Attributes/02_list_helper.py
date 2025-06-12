369d01c4a01a646373beeefa52c156de68b91fed
Madalena <mmadalenaandrade93@gmail.com>
2025-06-12 12:54:56 +0100

    Update 02_list_helper.py

--- Part-09-Advanced-Course-in-Programming/5_Class_Attributes/02_list_helper.py
+++ Part-09-Advanced-Course-in-Programming/5_Class_Attributes/02_list_helper.py
@@ -1,37 +1,29 @@
 # WRITE YOUR SOLUTION HERE:
+#model solution used @classmethod, but for me it makes more sense to use @staticmethod
 class ListHelper:
-
-    @classmethod
-    def greatest_frequency(cls, my_list: list):
-        checked = []
-        counts = []
+    @staticmethod
+    def greatest_frequency(my_list: list):
+        frequencies = []
         for item in my_list:
-            if item not in checked:
-                counts.append((item, my_list.count(item)))
-                checked.append(item)
-        greatest = 0
-        common_item = ""
-        for tupple in counts:
-            if tupple[1] > greatest:
-                greatest = tupple[1]
-                common_item = tupple[0]
-        return common_item
+            frequencies.append(my_list.count(item))
 
-    @classmethod
-    def doubles(cls, my_list: list):
-        checked = []
-        counts = []
-        for item in my_list:
-            if item not in checked:
-                counts.append((item, my_list.count(item)))
-                checked.append(item)
+        index_max = frequencies.index(max(frequencies))
+        return my_list[index_max]
+
+    @staticmethod
+    def doubles(my_list: list):
+        set_numbers = set(my_list) #used set because it filters repetitive numbers
+
         count = 0
-        for tupple in counts:
-            if tupple[1] >= 2:
+        for number in set_numbers:
+            if my_list.count(number) >= 2:
                 count += 1
+        
         return count
+        
 
-if __name__ == "__main__":
+
+if __name__=="__main__":
     numbers = [1, 1, 2, 1, 3, 3, 4, 5, 5, 5, 6, 5, 5, 5]
     print(ListHelper.greatest_frequency(numbers))
     print(ListHelper.doubles(numbers))
