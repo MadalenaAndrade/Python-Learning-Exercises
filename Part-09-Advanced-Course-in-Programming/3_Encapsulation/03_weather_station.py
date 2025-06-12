5377d0f05fcc1a18f9e3123552535e4b12471731
Madalena <mmadalenaandrade93@gmail.com>
2025-06-12 11:55:23 +0100

    Update 03_weather_station.py

--- Part-09-Advanced-Course-in-Programming/3_Encapsulation/03_weather_station.py
+++ Part-09-Advanced-Course-in-Programming/3_Encapsulation/03_weather_station.py
@@ -1,24 +1,21 @@
 # WRITE YOUR SOLUTION HERE:
 class WeatherStation:
-    def __init__(self, station: str):
-        self.__station = station
+    def __init__(self, name: str):
+        self.__name = name
         self.__observations = []
     
     def add_observation(self, observation: str):
         self.__observations.append(observation)
     
     def latest_observation(self):
-        last = ""
-        if self.__observations != []:
-            last = self.__observations[-1]
-        return last
+        return "" if not self.__observations else self.__observations[-1]
     
     def number_of_observations(self):
         return len(self.__observations)
 
     def __str__(self):
-        return f"{self.__station}, {len(self.__observations)} observations"
-    
+        return f"{self.__name}, {self.number_of_observations()} observations"
+
 if __name__=="__main__":
     station = WeatherStation("Houston")
     station.add_observation("Rain 10mm")
